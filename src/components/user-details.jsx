3a1657bb4fa2d8c8865daa3e44482ae74f7a763c
norberdo <norbert.suski@gmail.com>
2020-05-11 13:01:57 +0200

    Remove button added to user details

--- src/components/user-details.jsx
+++ src/components/user-details.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   margin-top: 20px;
+
+  button {
+    margin-top: 10px;
+    padding: 5px;
+  }
 `;
 
 const GET_USER = gql`
@@ -18,8 +23,19 @@ const GET_USER = gql`
   }
 `;
 
+const DELETE_USER = gql`
+  mutation DeleteUser($id: String!){
+    deleteUser(id: $id)
+  }
+`;
+
 const UserDetails = ({ id }) => {
   const { data } =  useQuery(GET_USER, { variables: { id }});
+  const [deleteUser] = useMutation(DELETE_USER);
+
+  const handleOnClick = ()=> {
+    deleteUser({ variables: { id }});
+  }
 
   if (data && data.user) {
     const { user: { firstname, lastname, nickname, email }} = data;
@@ -28,6 +44,7 @@ const UserDetails = ({ id }) => {
       <p>Name: {firstname} {lastname}</p>
       <p>Nickname: {nickname}</p>
       <p>E-mail: {email}</p>
+      <button onClick={handleOnClick}>Remove</button>
     </Wrapper>;
   }
 
